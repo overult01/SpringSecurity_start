0fbdcb4f92c3ab1c62d1ac5e0fb511b3f7e58bd7
it_st it <overult01@gmail.com>
2022-04-10 16:39:43 +0900

    first

---
+++ src/main/java/com/cos/security1/Ssecurity1Application.java
@@ -0,0 +1,13 @@
+package com.cos.security1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssecurity1Application.class, args);
+	}
+
+}
