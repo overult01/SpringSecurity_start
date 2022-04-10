ce73baa9262e9ef4958cc2d32297eb8b74fb6ba2
it_st it <overult01@gmail.com>
2022-04-10 16:41:40 +0900

    first

---
+++ src/test/java/com/cos/security1/Ssecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.security1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
