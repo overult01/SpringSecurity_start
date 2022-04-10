d5967ff17072858bad7032ef09a5ed600c87edce
it_st it <overult01@gmail.com>
2022-04-10 21:56:00 +0900

    BCryptPasswordEncoder: 회원가입시 비밀번호 암호화

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록 
+	public BCryptPasswordEncoder encodePwd() { // 비밀번호 암호화하려고 
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/user/**").authenticated() // 이런 주소로 들어오면 인증 필요(로그인 한 사람만 들어올 수 있다)
+		.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") // 로그인했지만 admin, manager만 들어올 수 있
+		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") // 로그인한 admin만 들어올 수 있다.
+		.anyRequest().permitAll() // (위 3개 외) 다른 요청은 모두 허용
+		.and()
+		.formLogin()
+		.loginPage("/loginform"); // 권한이 없는 페이지로 가려고 하면 login페이지로 보내기 
+	}
+}
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,12 +1,24 @@
 package com.cos.security1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
 
 @Controller // 뷰 반환
 public class IndexController {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@GetMapping({"", "/"})
 	public String index() {
 		// mustache(스프링에서 권장하는 템플릿엔진. jsp 대신)
@@ -15,10 +27,26 @@ public class IndexController {
 		return "index";
 	}
 	
-	// 디폴트 설정으로는 스프링 시큐리티가 가로챈다.(설정을 추후 바꿔주어야 한다)
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	// 디폴트 설정으로는 스프링 시큐리티가 가로챈다.(설정을 추후 바꿔주어야 한다) -> SecudrityConfig 파일 생성 후 스프링시큐리티 디폴트 login페이지 작동안함 
+	@GetMapping("/loginform")
+	public String login() {
+		return "loginForm";
+	}
+
+	@GetMapping("/joinform")
+	public String joinForm() {
+		return "joinForm";
+	}
+
+	@PostMapping("/join")
+	public String join(User user) {
+		System.out.println(user.getUsername() + " " + user.getPassword() + " " + user.getEmail());
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword); // bCryptPasswordEncoder: 비번 암호화 
+		user.setPassword(encPassword); // 인코딩된 password를 넣고 user정보를 save
+		userRepository.save(user); // 시큐리티로 로그인 불가: 패스워드가 암호화가 안되어서  -> 	BCryptPasswordEncoder로 해결 
+		return "redirect:/loginform";
 	}
 	
 	@GetMapping("/user")
@@ -35,15 +63,4 @@ public class IndexController {
 	public @ResponseBody String manager() {
 		return "manager";
 	}
-	
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
-	}
-	
-	@GetMapping("/joinProc")
-	public @ResponseBody String loginProc() {
-		return "회원가입 완료됨!";
-	}
-
 }
